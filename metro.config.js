5a8ef92a7a409df675e534e230d3f984fef5fc29
Michael Wood <esiotrot@gmail.com>
2023-05-31 18:19:02 +0200

    Remove references to nonexistent files

--- metro.config.js
+++ metro.config.js
@@ -1,20 +1,14 @@
-const { getDefaultConfig } = require("metro-config");
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require("expo/metro-config");
 
+const config = getDefaultConfig(__dirname);
 
-module.exports = (async () => { 
-	const {  
-		resolver: { 
-			sourceExts, 
-			assetExts 
-		}  
-	} = await getDefaultConfig(); 
+config.transformer.babelTransformerPath = require.resolve(
+  "react-native-svg-transformer"
+);
+config.resolver.assetExts = config.resolver.assetExts.filter(
+  (ext) => ext !== "svg"
+);
+config.resolver.sourceExts.push("svg");
 
-	return {
-		transformer: {      
-			babelTransformerPath: require.resolve("react-native-svg-transformer")    
-		},    
-		resolver: {
-			assetExts: assetExts.filter(ext => ext !== "svg"),
-			sourceExts: [...sourceExts, "svg"]    
-		}};
-})();
+module.exports = config;
